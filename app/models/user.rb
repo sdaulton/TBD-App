4f70e191d7791e5601925d538e13e90dc041eaf8
farahfouladi <ffouladi@colgate.edu>
2015-04-03 15:13:51 -0400

    changed google authentication, rediected views

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,11 @@ class User < ActiveRecord::Base
 
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info
-    user = User.where(:email => data["email"])
+    user = User.where(:email => data["email"]).first
+	 if user.nil?
+	   user = User.create!(name: data["name"],email: data["email"],password: Devise.friendly_token[0,20])
+    end
+    return user
   end
   
 end
