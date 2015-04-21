f9d8c7a982b054c20cc43492e58e9740f1fa0e95
Lauren Yeary <lyeary@colgate.edu>
2015-04-21 09:59:47 -0400

    Update 20150413212036_add_riders.rb

--- db/migrate/20150413212036_add_riders.rb
+++ db/migrate/20150413212036_add_riders.rb
@@ -1,5 +1,14 @@
 class AddRiders < ActiveRecord::Migration
   def change
     add_column :riders, :user_id, :integer
+    add_column :users, :is_rider, :boolean
+    add_column :users, :is_driver, :boolean
+    add_column :users, :driver_license, :string
+    add_column :users, :driver_license_state, :string
+    add_column :users, :license_plate, :string
+    add_column :users, :license_plate_state, :string
+    add_column :users, :make, :string
+    add_column :users, :model, :string
+    add_column :users, :year, :integer
   end
 end
